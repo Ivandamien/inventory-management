05d43ea884d5215bb3be939804aecb583dc706bf
Ivandamien <ivandamieno99@gmail.com>
2025-07-29 00:11:55 +0300

    Building the cardpopularproducts

--- client/src/app/dashboad/CardPopularProducts.tsx
+++ client/src/app/dashboad/CardPopularProducts.tsx
@@ -2,6 +2,7 @@
 import { useGetDashboardMetricsQuery } from "@/state/api";
 import { ShoppingBag } from "lucide-react";
 import React from "react";
+import Rating from "../(components)/Rating";
 
 interface Props {}
 
@@ -32,7 +33,7 @@ const CardPopularProducts = (props: Props) => {
                         Ksh {product.price}
                       </span>
                       <span className="mx-2">|</span>
-                      <div>rating</div>
+                      <Rating rating={product.rating || 0} />
                     </div>
                   </div>
                 </div>
