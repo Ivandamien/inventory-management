5ff1c2533794e7f6836329dabe1f3ec514759a88
Ivandamien <ivandamieno99@gmail.com>
2025-07-28 16:42:46 +0300

    feat:implementing api

---
+++ client/src/state/api.ts
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/",
+  }),
+  reducerPath: "api",
+  tagTypes: [],
+  endpoints: (build) => ({}),
+});
+
+export const {} = api;
